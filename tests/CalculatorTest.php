fd20b659307745f9811ae2778e3bca797d8f1ff5
Alpha Olomi <alphaolomi@gmail.com>
2022-06-03 21:03:29 +0300

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -4,7 +4,7 @@ use JasiriLabs\Calculator\Calculator;
 
 test('can return hello world', function () {
     $calc = new Calculator;
-        $this->assertEquals('Hello World', $calc->test());
+    $this->assertEquals('Hello World', $calc->test());
 });
 
 test('can sum numbers', function () {
@@ -12,7 +12,3 @@ test('can sum numbers', function () {
     $this->assertEquals(3, $calc->add(1,2));
 });
 
-test('can use facade', function () {
-    $calc = new Calculator;    
-    $this->assertEquals(3, $calc->add(1,2));
-});
